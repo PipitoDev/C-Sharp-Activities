a92d36b014888aa4630283dabd31b6521417529e
PipitoDev <joaovitorsguizzato@gmail.com>
2026-08-26 10:35:23 -0300

    Add files via upload

---
+++ Atividades Independentes/Pequena Lista/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+namespace MyProgramAgain;
+
+class FibonacciSequence
+{
+    public static void Main()
+    {
+        int num1 = 0, num2 = 1, num3 = (num1 + num2);
+        Console.WriteLine("A sequência de Fibonacci é:");
+        while (num1 < 50000)
+        {
+            Console.WriteLine(num1);
+            num1 = num2;
+            num2 = num3;
+            num3 = (num1 + num2);
+        }
+        Console.WriteLine("E assim por diante...");
+    }
+}
